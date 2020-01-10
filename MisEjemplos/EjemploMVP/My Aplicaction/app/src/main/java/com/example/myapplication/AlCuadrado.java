86fcbffcdebb8daa0c856cf135089133a187186b
Pablo Lopez <esviasalpa@gmail.com>
2020-01-10 18:16:36 +0100

    actualizacion

--- MisEjemplos/EjemploMVP/My Aplicaction/app/src/main/java/com/example/myapplication/AlCuadrado.java
+++ MisEjemplos/EjemploMVP/My Aplicaction/app/src/main/java/com/example/myapplication/AlCuadrado.java
@@ -16,20 +16,13 @@ public interface AlCuadrado {
     }
 
     interface  Presenter{
-        /*El Result que manda el resultado final calculado por el Model o Modelo(AlCuadradoModel) a la Vista o View(AlCuadradoView)
-        * AlCuadradoModel -> AlCuadradoPresenter -> AlCuadradoView*/
-        void showReulst(String result);
+
         /* El Presenter Recoge el dato a calcular de la vista para mandarselo a el modelo
          AlCuadradoView -> AlcuadradoPresenter -> AlCuadradoModel*/
         void alCuadrado(String data);
-        /*El Presenter informa a la visata de que el Model no pudo realizar el calculo al producirse un error
-        * AlCuadradoModel -> AlCuadradoPresenter -> AlCuadradoView*/
-        void showError(String error);
+
+
+
     }
 
-    interface  Model{
-        /*El Modelo Calcula el cuadrado de el dato recibido y lo carga en el presenter para que se lo mande a la vista, es decir usara el showResult del Presenter
-        * AlCuadradoModel -> AlCuadradoPresenter -> AlCuadradoView*/
-        void alCuadrado(String data);
-    }
 }
--- MisEjemplos/EjemploMVP/My Aplicaction/app/src/main/java/com/example/myapplication/AlcuadradoPresenter.java
+++ MisEjemplos/EjemploMVP/My Aplicaction/app/src/main/java/com/example/myapplication/AlcuadradoPresenter.java
@@ -6,40 +6,29 @@ public class AlcuadradoPresenter implements AlCuadrado.Presenter {
     /*Declaracion de interfaz de la vista y del modelo para que el Presenter
     * pueda realiza la comunicacion entre estas dos accediendo a estas a través de sus interfaces*/
     private  AlCuadrado.View view;
-    private  AlCuadrado.Model model;
+
 
     //Constructor de la clase
     public  AlcuadradoPresenter(AlCuadrado.View view){
         this.view=view;
-        model= new AlCuadradoModel(this);
-
 
     }
 
-    @Override
-    public void showReulst(String result) {
-        if(view!=null)
-        {
-            //Llamamos al metodo que hay dentro de la vista, es dedcir que implementa la vista, pero a traves de la interfaz es decir  AlCuadrado.View
-            view.showResult(result);
-        }
 
 
-    }
+
+
 
     /*El Presenter comunica el dato a calcular a AlcuadradoModel a través tambien de la interfaz que implementa "AlCuadrado.Model"  como en el metodo showReuslts(String result)*/
     @Override
     public void alCuadrado(String data) {
 
-        if(view!=null)
-        {
-          model.alCuadrado(data);
-        }
+        if(data == null)
+            view.showError("ERROR");
+
+        //calculo
+
     }
-/*El presente comunica el Error al calcula producido en el Modelo(AlCuadradoModel) a la vista(AlCuadradoView)*/
-    @Override
-    public void showError(String error) {
-        if(view!=null)
-            view.showError(error);
-    }
+
+
 }
